7732e68dd349f79d92d354215960fe5bc4a9dc2e
Rharhuky <rharhuky@gmail.com>
2024-06-19 23:52:54 -0300

    add: Student, SchoolDiscipline

---
+++ src/main/java/com/rharhuky/api/domain/Student.java
@@ -0,0 +1,31 @@
+package com.rharhuky.api.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "student")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "birthday")
+    private LocalDate birthday;
+
+    @Column(name = "registration")
+    private String registration;
+
+}
